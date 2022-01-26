ede95e9775b0c66c39d36a37a3a407c51b8b14a3
vineetha-batchu <batchuvinetha@gmail.com>
2022-01-26 14:44:28 -0600

    Initial

---
+++ 01mapper.py
@@ -0,0 +1,15 @@
+#Vineetha Batchu
+#Practice Mapper
+
+f = open("purchases.txt","r")  # open file, read-only
+o = open("a.txt", "w") # open file, write
+for line in f:  
+    rowList = line.strip().split("    ") 
+    print (rowList )
+    print (len(rowList ))
+    if len(rowList) == 6:
+        date, time, location, dept, amount, payType = rowList  #assign names
+        print ("{0}\t{1}".format(location, amount))
+        o.write("{0}\t{1}\n".format(location, amount))
+f.close()
+o.close()
